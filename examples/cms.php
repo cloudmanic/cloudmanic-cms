0e5c4a0d6044aad04fb4e9c23125cb0d101323e1
Cloudmanic Labs, LLC <github@cloudmanic.com>
2012-10-20 23:28:29 -0700

    Added example config files

---
+++ examples/cms.php
@@ -0,0 +1,19 @@
+<?php
+//
+// Company: Cloudmanic Labs, LLC
+// Website: http://cloudmanic.com
+// Date: 10/18/2012
+//
+
+return array(
+	// Database
+	'db_host' => 'localhost',
+	'db_database' => 'cms',
+	'db_username' => 'root',
+	'db_password' => '',
+
+	// CMS Configs
+	'table_base' => 'CMS_'
+);  
+
+/* End File */
